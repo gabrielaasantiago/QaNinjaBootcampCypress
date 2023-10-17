ef9710a855fa99e4d6dfbdfcb419b2f5bcfb4ca3
gabrielaasantiago <gabrielaasantiago@outlook.com>
2023-10-17 14:36:06 -0300

    removendo allure

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -1,16 +1,11 @@
 const { defineConfig } = require('cypress')
 const { cypressBrowserPermissionsPlugin } = require('cypress-browser-permissions')
-const allureWriter = require('@shelex/cypress-allure-plugin/writer')
 
-const mongo = require('cypress-mongodb')
 require('dotenv').config()
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-
-      allureWriter(on, config)
-      mongo.configurePlugin(on)
 
       config = cypressBrowserPermissionsPlugin(on, config)
       
@@ -26,11 +21,6 @@ module.exports = defineConfig({
       browserPermissions: {
         notifications: 'allow',
         geolocation: 'allow'
-      },
-
-      mongodb: {
-        uri: "mongodb+srv://gabrielaasantiago:xizm57c7ZIdZdzKh@gabiversion.li1ik56.mongodb.net/QTruckDB?retryWrites=true&w=majority",
-        database: 'QTruckDB'
       }
     }
   },
--- tests/cypress/support/e2e.js
+++ tests/cypress/support/e2e.js
@@ -13,12 +13,10 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-import '@shelex/cypress-allure-plugin'
+
 // Import commands.js using ES2015 syntax:
 import './commands'
 
-const mongo = require('cypress-mongodb')
-mongo.addCommands()
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
